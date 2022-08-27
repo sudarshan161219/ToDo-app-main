96f631c0bab1e1499a7a7eb74792873bd08f4e13
Sudarshan Hosalli@DESKTOP-F068LQM <godhustler90956@gmail.com>
2022-08-28 01:23:29 +0530

    update

--- app.js
+++ app.js
@@ -1,25 +1,56 @@
 const inputTodo = document.querySelector(".todo-input");
-const  addTodo = document.querySelector(".circle")
-
-const element = document.querySelector(".todo-list")
+const  addTodo = document.querySelector(".circle");
+const element = document.querySelector(".todo-list");
 
 
 inputTodo.addEventListener("change", (e)=> {
-    var tag = document.createElement("li");
-    var text = document.createTextNode(inputTodo.value);
-    tag.appendChild(text);
-    element.appendChild(tag);
-    inputTodo.value = ''
-})
+  var check = document.createElement("img");
+ var tag = document.createElement("li");
+ var tick = document.createElement("span");
+ var img = document.createElement("img");
+ var text = document.createTextNode(inputTodo.value);
 
 
-addTodo.addEventListener("click", ()=> {
-  var tag = document.createElement("li");
-   var text = document.createTextNode(inputTodo.value);
+  tag.appendChild(tick);
+  tag.appendChild(text);
+  tag.appendChild(img);
+  element.appendChild(tag);
+  inputTodo.value = '';
+
+ 
+   img.src= "images/icon-cross.svg" 
+  
+
+
+
+
+
+
+   addTodo.addEventListener("click", (e)=> {
    tag.appendChild(text);
+   tag.appendChild(img);
    element.appendChild(tag);
-   inputTodo.value = ''
+   inputTodo.value = '';
 
-})
+});
+
+
+img.addEventListener("click", ()=> {
+  tag.classList.add("hide-li");
+});
+
+tick.addEventListener("click", (e)=> {
+tick.classList.add("tick")
+tag.classList.add("tick-li")
+  tag.appendChild(check)
+  check.src="images/icon-check.svg"
+  check.classList.add("check-img");
+});
+
+});
+
+
+
+
 
 
